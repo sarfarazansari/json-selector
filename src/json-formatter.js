d398f207a8b429f6c92cfe7b6d736d9d47ced469
Piyush Sonagara <piyush@walkover.in>
2016-07-15 14:35:19 +0530

    feat: event handling service
    option to register custom callbacks when an element is selected/deselected

--- src/json-formatter.js
+++ src/json-formatter.js
@@ -52,205 +52,239 @@ angular.module('jsonFormatter', ['RecursionHelper'])
   };
 })
 
-.directive('jsonFormatter', ['RecursionHelper', 'JSONFormatterConfig', function jsonFormatterDirective(RecursionHelper, JSONFormatterConfig) {
-  function escapeString(str) {
-    return str.replace('"', '\"');
-  }
+// Proxy services for events as per http://stackoverflow.com/a/29537535
+.service('$jsonFormatter', [function() {
+  var listeners = [];
 
-  // From http://stackoverflow.com/a/332429
-  function getObjectName(object) {
-    if (object === undefined) {
-      return '';
-    }
-    if (object === null) {
-      return 'Object';
-    }
-    if (typeof object === 'object' && !object.constructor) {
-        return 'Object';
-    }
-    var funcNameRegex = /function (.{1,})\(/;
-    var results = (funcNameRegex).exec((object).constructor.toString());
-    if (results && results.length > 1) {
-      return results[1];
-    } else {
-      return '';
-    }
-  }
-
-  function getType(object) {
-    if (object === null) { return 'null'; }
-    return typeof object;
-  }
-
-  function getValuePreview (object, value) {
-    var type = getType(object);
-
-    if (type === 'null' || type === 'undefined') { return type; }
-
-    if (type === 'string') {
-      value = '"' + escapeString(value) + '"';
-    }
-    if (type === 'function'){
-
-      // Remove content of the function
-      return object.toString()
-          .replace(/[\r\n]/g, '')
-          .replace(/\{.*\}/, '') + '{…}';
-
-    }
-    return value;
-  }
-
-  function getPreview(object) {
-    var value = '';
-    if (angular.isObject(object)) {
-      value = getObjectName(object);
-      if (angular.isArray(object))
-        value += '[' + object.length + ']';
-    } else {
-      value = getValuePreview(object, object);
-    }
-    return value;
-  }
-
-  function link(scope) {
-    scope.isArray = function () {
-      return angular.isArray(scope.json);
-    };
-
-    scope.isObject = function() {
-      return angular.isObject(scope.json);
-    };
-
-    scope.getKeys = function (){
-      if (scope.isObject()) {
-        return Object.keys(scope.json).map(function(key) {
-          if (key === '') { return '""'; }
-          return key;
-        });
+  var broadcastEvent = function(event, data) {
+    listeners.forEach(function(callback) {
+      if (angular.isFunction(callback)) {
+        callback(event, data);
       }
-    };
-    scope.type = getType(scope.json);
-    scope.hasKey = typeof scope.key !== 'undefined';
-    scope.getConstructorName = function(){
-      return getObjectName(scope.json);
-    };
-
-    if (scope.type === 'string'){
-
-      // Add custom type for date
-      if((new Date(scope.json)).toString() !== 'Invalid Date') {
-        scope.isDate = true;
-      }
-
-      // Add custom type for URLs
-      if (scope.json.indexOf('http') === 0) {
-        scope.isUrl = true;
-      }
-    }
-
-    scope.isEmptyObject = function () {
-      return scope.getKeys() && !scope.getKeys().length &&
-        scope.isOpen && !scope.isArray();
-    };
-
-
-    // If 'open' attribute is present
-    scope.isOpen = !!scope.open;
-    scope.toggleOpen = function () {
-      scope.isOpen = !scope.isOpen;
-    };
-    scope.childrenOpen = function () {
-      if (scope.open > 1){
-        return scope.open - 1;
-      }
-      return 0;
-    };
-
-    scope.openLink = function (isUrl) {
-      if(isUrl) {
-        window.location.href = scope.json;
-      }
-    };
-
-    scope.parseValue = function (value){
-      return getValuePreview(scope.json, value);
-    };
-
-    scope.showThumbnail = function () {
-      return !!JSONFormatterConfig.hoverPreviewEnabled && scope.isObject() && !scope.isOpen;
-    };
-
-    scope.getThumbnail = function () {
-      if (scope.isArray()) {
-
-        // if array length is greater then 100 it shows "Array[101]"
-        if (scope.json.length > JSONFormatterConfig.hoverPreviewArrayCount) {
-          return 'Array[' + scope.json.length + ']';
-        } else {
-          return '[' + scope.json.map(getPreview).join(', ') + ']';
-        }
-      } else {
-
-        var keys = scope.getKeys();
-
-        // the first five keys (like Chrome Developer Tool)
-        var narrowKeys = keys.slice(0, JSONFormatterConfig.hoverPreviewFieldCount);
-
-        // json value schematic information
-        var kvs = narrowKeys
-          .map(function (key) { return key + ':' + getPreview(scope.json[key]); });
-
-        // if keys count greater then 5 then show ellipsis
-        var ellipsis = keys.length >= 5 ? '…' : '';
-
-        return '{' + kvs.join(', ') + ellipsis + '}';
-      }
-    };
-
-    scope.elementsSelectable = function () {
-      return !!JSONFormatterConfig.elementsSelectable;
-    };
-
-    scope.getExpression = function() {
-      var current = scope;
-      var parent = current.parent;
-      var path = "";
-
-      if (parent && parent.json) {
-        do {
-          if (angular.isArray(parent.json)) {
-            path = "[" + current.key + "]" + path;
-          } else {
-            path = "." + current.key + path;
-          }
-
-          current = parent;
-          parent = parent.parent;
-        } while (parent !== undefined);
-      }
-
-      console.log(path);
-    };
-  }
+    });
+  };
 
   return {
-    templateUrl: 'json-formatter.html',
-    restrict: 'E',
-    replace: true,
-    scope: {
-      json: '=',
-      key: '=',
-      open: '=',
-      parent: '='
+    select: function(expression) {
+      broadcastEvent('element.select', expression);
     },
-    compile: function(element) {
-
-      // Use the compile function from the RecursionHelper,
-      // And return the linking function(s) which it returns
-      return RecursionHelper.compile(element, link);
+    deselect: function(expression) {
+      broadcastEvent('element.deselect', expression);
+    },
+    register: function(callback) {
+      listeners.push(callback);
     }
   };
+}])
+
+.directive('jsonFormatter', ['RecursionHelper', 'JSONFormatterConfig', '$jsonFormatter',
+  function (RecursionHelper, JSONFormatterConfig, $jsonFormatter) {
+
+    function escapeString(str) {
+      return str.replace('"', '\"');
+    }
+
+    // From http://stackoverflow.com/a/332429
+    function getObjectName(object) {
+      if (object === undefined) {
+        return '';
+      }
+      if (object === null) {
+        return 'Object';
+      }
+      if (typeof object === 'object' && !object.constructor) {
+          return 'Object';
+      }
+      var funcNameRegex = /function (.{1,})\(/;
+      var results = (funcNameRegex).exec((object).constructor.toString());
+      if (results && results.length > 1) {
+        return results[1];
+      } else {
+        return '';
+      }
+    }
+
+    function getType(object) {
+      if (object === null) { return 'null'; }
+      return typeof object;
+    }
+
+    function getValuePreview (object, value) {
+      var type = getType(object);
+
+      if (type === 'null' || type === 'undefined') { return type; }
+
+      if (type === 'string') {
+        value = '"' + escapeString(value) + '"';
+      }
+      if (type === 'function'){
+
+        // Remove content of the function
+        return object.toString()
+            .replace(/[\r\n]/g, '')
+            .replace(/\{.*\}/, '') + '{…}';
+
+      }
+      return value;
+    }
+
+    function getPreview(object) {
+      var value = '';
+      if (angular.isObject(object)) {
+        value = getObjectName(object);
+        if (angular.isArray(object))
+          value += '[' + object.length + ']';
+      } else {
+        value = getValuePreview(object, object);
+      }
+      return value;
+    }
+
+    function link(scope) {
+      scope.isArray = function () {
+        return angular.isArray(scope.json);
+      };
+
+      scope.isObject = function() {
+        return angular.isObject(scope.json);
+      };
+
+      scope.getKeys = function (){
+        if (scope.isObject()) {
+          return Object.keys(scope.json).map(function(key) {
+            if (key === '') { return '""'; }
+            return key;
+          });
+        }
+      };
+      scope.type = getType(scope.json);
+      scope.hasKey = typeof scope.key !== 'undefined';
+      scope.getConstructorName = function(){
+        return getObjectName(scope.json);
+      };
+
+      if (scope.type === 'string'){
+
+        // Add custom type for date
+        if((new Date(scope.json)).toString() !== 'Invalid Date') {
+          scope.isDate = true;
+        }
+
+        // Add custom type for URLs
+        if (scope.json.indexOf('http') === 0) {
+          scope.isUrl = true;
+        }
+      }
+
+      scope.isEmptyObject = function () {
+        return scope.getKeys() && !scope.getKeys().length &&
+          scope.isOpen && !scope.isArray();
+      };
+
+
+      // If 'open' attribute is present
+      scope.isOpen = !!scope.open;
+      scope.toggleOpen = function () {
+        scope.isOpen = !scope.isOpen;
+      };
+      scope.childrenOpen = function () {
+        if (scope.open > 1){
+          return scope.open - 1;
+        }
+        return 0;
+      };
+
+      scope.openLink = function (isUrl) {
+        if(isUrl) {
+          window.location.href = scope.json;
+        }
+      };
+
+      scope.parseValue = function (value){
+        return getValuePreview(scope.json, value);
+      };
+
+      scope.showThumbnail = function () {
+        return !!JSONFormatterConfig.hoverPreviewEnabled && scope.isObject() && !scope.isOpen;
+      };
+
+      scope.getThumbnail = function () {
+        if (scope.isArray()) {
+
+          // if array length is greater then 100 it shows "Array[101]"
+          if (scope.json.length > JSONFormatterConfig.hoverPreviewArrayCount) {
+            return 'Array[' + scope.json.length + ']';
+          } else {
+            return '[' + scope.json.map(getPreview).join(', ') + ']';
+          }
+        } else {
+
+          var keys = scope.getKeys();
+
+          // the first five keys (like Chrome Developer Tool)
+          var narrowKeys = keys.slice(0, JSONFormatterConfig.hoverPreviewFieldCount);
+
+          // json value schematic information
+          var kvs = narrowKeys
+            .map(function (key) { return key + ':' + getPreview(scope.json[key]); });
+
+          // if keys count greater then 5 then show ellipsis
+          var ellipsis = keys.length >= 5 ? '…' : '';
+
+          return '{' + kvs.join(', ') + ellipsis + '}';
+        }
+      };
+
+      scope.elementsSelectable = function () {
+        return !!JSONFormatterConfig.elementsSelectable;
+      };
+
+      scope.isSelected = !!scope.selected;
+      scope.toggleSelectElement = function() {
+        var current = scope;
+        var parent = current.parent;
+        var expression = "";
+
+        if (parent && parent.json) {
+          do {
+            if (angular.isArray(parent.json)) {
+              expression = "[" + current.key + "]" + expression;
+            } else {
+              expression = "." + current.key + expression;
+            }
+
+            current = parent;
+            parent = parent.parent;
+          } while (parent !== undefined);
+        }
+
+        if (!scope.isSelected) {
+          $jsonFormatter.select(expression);
+        } else {
+          $jsonFormatter.deselect(expression);
+        }
+
+        scope.isSelected = !scope.isSelected;
+      };
+    }
+
+    return {
+      templateUrl: 'json-formatter.html',
+      restrict: 'E',
+      replace: true,
+      scope: {
+        json: '=',
+        key: '=',
+        open: '=',
+        parent: '='
+      },
+      compile: function(element) {
+
+        // Use the compile function from the RecursionHelper,
+        // And return the linking function(s) which it returns
+        return RecursionHelper.compile(element, link);
+      }
+    };
 }]);
 
 // Export to CommonJS style imports. Exporting this string makes this valid:
